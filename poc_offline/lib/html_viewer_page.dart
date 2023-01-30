aba2a2f4b7c85e84948ab88f514f7eaadd835aca
Rafael Hidalgo <rhidalgo@axpe.com>
2023-01-30 14:07:34 +0100

    WIP: Windows working.

--- poc_offline/lib/html_viewer_page.dart
+++ poc_offline/lib/html_viewer_page.dart
@@ -1,6 +1,10 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+import 'package:webview_windows/webview_windows.dart';
 
 class HTMLViewerPage extends StatefulWidget {
   final String fileHtmlContents;
@@ -19,6 +23,30 @@ class HTMLViewerPage extends StatefulWidget {
 class _HTMLViewerPageState extends State<HTMLViewerPage> {
   @override
   Widget build(BuildContext context) {
+    if (Platform.isWindows) {
+      return _getWindows();
+    }
+    if (kIsWeb) {
+      return _getWeb();
+    }
+    return _getMobile();
+  }
+
+  Widget _getWindows() {
+    final controller = WebviewController();
+    controller.initialize();
+    controller.loadUrl(widget.path);
+
+    return Scaffold(
+      body: Webview(controller),
+    );
+  }
+
+  Widget _getWeb() {
+    return Container();
+  }
+
+  Widget _getMobile() {
     late final PlatformWebViewControllerCreationParams params;
     if (WebViewPlatform.instance is WebKitWebViewPlatform) {
       params = WebKitWebViewControllerCreationParams(
@@ -32,9 +60,9 @@ class _HTMLViewerPageState extends State<HTMLViewerPage> {
     final WebViewController controller =
         WebViewController.fromPlatformCreationParams(params);
     controller
-    ..enableZoom(false)
-    ..loadFlutterAsset(widget.path)
-    ..setJavaScriptMode(JavaScriptMode.unrestricted);
+      ..enableZoom(false)
+      ..loadFlutterAsset(widget.path)
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
 
     return Scaffold(
       body: WebViewWidget(controller: controller),
