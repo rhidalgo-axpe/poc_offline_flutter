d5cd08cb61bc7da50f2699d561df525dbffc3ef3
Rafael Hidalgo <rhidalgo@axpe.com>
2023-02-03 16:19:27 +0100

    Added web html viewer.

--- poc_offline/lib/html_viewer_page.dart
+++ poc_offline/lib/html_viewer_page.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'dart:ui' as ui;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
@@ -19,6 +22,17 @@ class HTMLViewerPage extends StatefulWidget {
 class _HTMLViewerPageState extends State<HTMLViewerPage> {
   @override
   Widget build(BuildContext context) {
+    if (kIsWeb) {
+      // ignore: undefined_prefixed_name
+      ui.platformViewRegistry.registerViewFactory(widget.path, (int viewId) {
+        return IFrameElement()
+          ..style.width = '100%'
+          ..style.height = '100%'
+          ..src = widget.path
+          ..style.border = 'none';
+      });
+      return HtmlElementView(viewType: widget.path);
+    }
     late final PlatformWebViewControllerCreationParams params;
     if (WebViewPlatform.instance is WebKitWebViewPlatform) {
       params = WebKitWebViewControllerCreationParams(
