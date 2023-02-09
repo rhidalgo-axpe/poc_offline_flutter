28f0936a661e941807fb23c1ae89773b4efc7fd7
Rafael Hidalgo <rhidalgo@axpe.com>
2023-02-09 10:12:02 +0100

    Added html viewer in windows.

--- poc_offline/lib/html_viewer_page.dart
+++ poc_offline/lib/html_viewer_page.dart
@@ -2,9 +2,12 @@ import 'dart:io' show Platform;
 
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 import 'package:webview_windows/webview_windows.dart';
+import 'package:path/path.dart' as p;
+
 
 class HTMLViewerPage extends StatefulWidget {
   final String fileHtmlContents;
@@ -21,10 +24,18 @@ class HTMLViewerPage extends StatefulWidget {
 }
 
 class _HTMLViewerPageState extends State<HTMLViewerPage> {
+  final _controller = WebviewController();
+
+  @override
+  void initState() {
+    initPlatformState(widget.path);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     if (Platform.isWindows) {
-      return _getWindows();
+      return _getWindows(_controller);
     }
     if (kIsWeb) {
       return _getWeb();
@@ -32,20 +43,69 @@ class _HTMLViewerPageState extends State<HTMLViewerPage> {
     return _getMobile();
   }
 
-  Widget _getWindows() {
-    final controller = WebviewController();
-    controller.initialize();
-    controller.loadUrl(widget.path);
-
+  // Windows
+  Widget _getWindows(WebviewController controller) {
     return Scaffold(
-      body: Webview(controller),
+      appBar: AppBar(
+          title: StreamBuilder<String>(
+        stream: _controller.title,
+        builder: (context, snapshot) {
+          return Text(
+              widget.path);
+        },
+      )),
+      body: Center(
+        child: compositeView(),
+      ),
     );
   }
 
+  Future<void> initPlatformState(String url) async {
+    if (Platform.isWindows) {
+      try {
+        await _controller.initialize();
+
+        await _controller.setBackgroundColor(Colors.transparent);
+        await _controller.setPopupWindowPolicy(WebviewPopupWindowPolicy.deny);
+        await _controller.loadUrl(getAssetFileUrl(url));
+        _controller.webMessage.listen((event) {
+          print(event);
+        });
+
+        if (!mounted) return;
+        setState(() {});
+      } on PlatformException catch (e) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          Container();
+        });
+      }
+    }
+  }
+
+  String getAssetFileUrl(String asset) {
+    final assetsDirectory = p.join(p.dirname(Platform.resolvedExecutable),
+        'data', 'flutter_assets', asset);
+    return Uri.file(assetsDirectory).toString();
+  }
+
+  Widget compositeView() {
+    return Webview(
+      _controller,
+      permissionRequested: _onPermissionRequested,
+    );
+  }
+
+  Future<WebviewPermissionDecision> _onPermissionRequested(
+      String url, WebviewPermissionKind kind, bool isUserInitiated) async {
+    return WebviewPermissionDecision.allow;
+  }
+
+  // Web
   Widget _getWeb() {
     return Container();
   }
 
+  // Mobile
   Widget _getMobile() {
     late final PlatformWebViewControllerCreationParams params;
     if (WebViewPlatform.instance is WebKitWebViewPlatform) {
